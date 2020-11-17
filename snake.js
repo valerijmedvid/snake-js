119be177b47556bad1c9300f8a5955bdff18f904
Valerij Medviď <vmedvid@gmail.com>
2020-11-17 20:30:36 +0100

    snake

--- snake.js
+++ snake.js
@@ -7,32 +7,40 @@ const ctx = canvas.getContext("2d")
 const title = document.querySelector('h1')
 
 // Game
-const snakeSize = 30
+let gameIsRunning = true
+
+const tileSize = 30
 let gameSpeed = 10
 let score = 0
 
 // Player
-let snakeSpeed = snakeSize
+let snakeSpeed = tileSize
+let snakeLength = 3
 let snakePosX = 0
 let snakePosY = canvas.height / 2
 
 let velocityX = 1
 let velocityY = 0
 
+let tail = []
+
 // Food
 let foodPosX = 0
 let foodPosY = 0
 
-const tileCountX = canvas.width / snakeSize
-const tileCountY = canvas.height / snakeSize
+const tileCountX = canvas.width / tileSize
+const tileCountY = canvas.height / tileSize
 
 
 /**
 * GAME LOOP
 */
 function gameLoop() {
-    drawStuff()
-    moveStuff()
+    if (gameIsRunning) {
+        drawStuff()
+        moveStuff()
+
+    }
 
     setTimeout(gameLoop, 1000 / gameSpeed)
 }
@@ -49,22 +57,35 @@ function moveStuff() {
     snakePosY += snakeSpeed * velocityY
 
     // Wall collision
-    if (snakePosX > canvas.width - snakeSize) {
+    if (snakePosX > canvas.width - tileSize) {
         snakePosX = 0
     }
     if (snakePosX < 0) {
         snakePosX = canvas.width
     }
-    if (snakePosY > canvas.height - snakeSize) {
+    if (snakePosY > canvas.height - tileSize) {
         snakePosY = 0
     }
     if (snakePosY < 0) {
         snakePosY = canvas.height
     }
 
+
+    // Game over
+    tail.forEach(snakePart => {
+        if (snakePosX === snakePart.x && snakePosY === snakePart.y) {
+            gameOver()
+        }
+    })
+
+    // Tail
+    tail.push({ x: snakePosX, y: snakePosY })
+    tail = tail.slice(-1 * snakeLength)
+
     // Food collision
     if (snakePosX === foodPosX && snakePosY === foodPosY) {
         gameSpeed++
+        snakeLength++
         title.textContent = ++score
         resetFood()
     }
@@ -82,10 +103,15 @@ function drawStuff() {
     drawGrid()
 
     // Food
-    rectangle("#00bfff", foodPosX, foodPosY, snakeSize, snakeSize)
+    rectangle("#00bfff", foodPosX, foodPosY, tileSize, tileSize)
+
+    // Tail
+    tail.forEach(snakePart => {
+        rectangle("#555", snakePart.x, snakePart.y, tileSize, tileSize)
+    })
 
     // Snake
-    rectangle("black", snakePosX, snakePosY, snakeSize, snakeSize)
+    rectangle("black", snakePosX, snakePosY, tileSize, tileSize)
 }
 
 // Draw rectangle
@@ -98,17 +124,34 @@ function rectangle(color, x, y, width, height) {
 function drawGrid() {
     for (let i = 0; i < tileCountX; ++i) {
         for (let j = 0; j < tileCountY; ++j) {
-            rectangle("#fff", snakeSize * i, snakeSize * j, snakeSize - 1, snakeSize - 1)
+            rectangle("#fff", tileSize * i, tileSize * j, tileSize - 1, tileSize - 1)
         }
     }
 }
 
 // Randomize food position
 function resetFood() {
-    foodPosX = Math.floor(Math.random() * tileCountX) * snakeSize
-    foodPosY = Math.floor(Math.random() * tileCountY) * snakeSize
+    if (snakeLength === tileCountX * tileCountY) {
+        gameOver()
+    }
+
+    foodPosX = Math.floor(Math.random() * tileCountX) * tileSize
+    foodPosY = Math.floor(Math.random() * tileCountY) * tileSize
+
+    // Don't spawn food on snake head
+    if (foodPosX === snakePosX && foodPosY === snakePosY) {
+        resetFood()
+    }
+    // Don't spawn food on any snake part
+    if (tail.some(snakePart => snakePart.x === foodPosX && snakePart.y === foodPosY)) {
+        resetFood()
+    }
 }
 
+function gameOver() {
+    title.innerHTML = `Game over. You score is <strong> ${score} </strong> `
+    gameIsRunning = false
+}
 
 /**
  *  KEYBOARD
@@ -140,6 +183,9 @@ function keyPush(event) {
                 velocityX = 0
                 velocityY = 1
             }
+            break
+        default:
+            if (!gameIsRunning) location.reload()
             break
     }
 }
